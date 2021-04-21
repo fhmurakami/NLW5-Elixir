2b637da58367cb0fc60961a49a6a9a2cd812e5e2
Felipe Murakami <felipe.murakami@locaweb.com.br>
2021-04-20 21:57:55 -0300

    Aula 01

---
+++ lib/inmana/welcomer.ex
@@ -0,0 +1,28 @@
+defmodule Inmana.Welcomer do
+  # Receber um nome e uma idade do usuario
+  # Se o usuário chamar "banana" e tiver idade "42", ele recebe uma mensagem especial
+  # Se o usuário for maior de idade, ele recebe uma mensagem normal
+  # Se o usuário for menor de idade, retornamos um erro
+  # Temos que tratar o nome do usuário para entradas erradas, como "BaNaNa", "BaNaNa \n"
+
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> evaluate(age)
+  end
+
+  defp evaluate("banana", 42) do
+    {:ok, "You are very special banana"}
+  end
+
+  defp evaluate(name, age) when age >= 18 do
+    {:ok, "Welcome #{name}"}
+  end
+
+  defp evaluate(name, _age) do
+    {:error, "You shall not pass #{name}"}
+  end
+end
